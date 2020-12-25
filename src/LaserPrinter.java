d6eb4c7b9c2642fcfc76ca6c1c8d0a3b29b09ee8
hasalfernando <witty.leo98@gmail.com>
2020-12-26 01:58:45 +0530

    Added Student attributes and run method of thread

--- src/LaserPrinter.java
+++ src/LaserPrinter.java
@@ -1,15 +1,23 @@
 public class LaserPrinter implements ServicePrinter {
 
+    //Monitor Class
+
     private int printerId;
     private int paperLevel;
     private int tonerLevel;
     private int noOfPrintedDocs;
+    private ThreadGroup students;
 
-    public LaserPrinter(int printerId){
+    public LaserPrinter(int printerId, ThreadGroup students){
+
         this.printerId = printerId;
+        this.paperLevel = ServicePrinter.Full_Paper_Tray;
+        this.tonerLevel = ServicePrinter.Full_Toner_Level;
+        this.noOfPrintedDocs = 0;
+        this.students = students;
     }
 
-    public void printDocument(Document document){
+    public synchronized void printDocument(Document document){
         if(this.paperLevel >= document.getNumberOfPages() && this.tonerLevel >= document.getNumberOfPages()){
             System.out.println(document);
             this.paperLevel = this.paperLevel- document.getNumberOfPages();
@@ -21,13 +29,26 @@ public class LaserPrinter implements ServicePrinter {
 
     }
 
+    @Override
     public void refillPaper( ){
-
+        while((this.paperLevel+ServicePrinter.SheetsPerPack) > ServicePrinter.Full_Paper_Tray){
+            try {
+                if (hasOperativeStudents()) {
+                    wait(5000);
+                }
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
     }
 
+    @Override
     public String toString(){
-        return "PrinterID: "+ this.printerId + "Paper Level: "+ this.paperLevel + "Toner Level: " + this.tonerLevel
-                + "Documents Printed: " + this.noOfPrintedDocs;
+        return new String("[ PrinterID: "+ this.printerId + "Paper Level: "+ this.paperLevel + "Toner Level: " + this.tonerLevel
+                + "Documents Printed: " + this.noOfPrintedDocs + " ]");
     }
 
+    private boolean hasOperativeStudents(){
+        return (students.activeCount()>0);
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,20 @@
-public class Student {
+public class Student extends Thread {
+
+    private String name;
+    private LaserPrinter printer;
+    private ThreadGroup group;
+
+    public Student(String name, LaserPrinter printer, ThreadGroup group){
+        this.name = name;
+        this.printer = printer;
+        this.group = group;
+    }
+
+    @Override
+    public void run() {
+
+        for(int i = 0; i<5; i++){
+            Document doc = new Document(this.getName(), "Doc-"+i, (int)Math.random()*1000);
+        }
+    }
 }
